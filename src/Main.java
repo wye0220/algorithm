2e5774d2f2ece2ae55a1b966f82cf8c2f8683595
Wen Ye <wye0220@gmail.com>
2018-12-20 02:55:29 -0500

    leetcode # 7

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import leetcode.Solution;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(String.format("Reverse Integer %s", solution.reverse(123)));
+
+
+    }
+}
